2b229050f4686dcb26eae063b78b2ee71522fa75
buriihenry <buriihenry@gmail.com>
2022-08-06 16:01:30 +0300

    Added code

--- week_1/docker_sql/Untitled-1.sh
+++ week_1/docker_sql/Untitled-1.sh
@@ -28,7 +28,7 @@ docker run -it \
   -v $(pwd)/ny_taxi_postgres_data/:/var/lib/postgresql/data \
   -p 5432:5432 \
   postgres:13
-
+#docker
 winpty docker run -it \
     -e POSTGRES_USER="root" \
     -e POSTGRES_PASSWORD="root" \
