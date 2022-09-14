ae82006275bb5cc903c0443fe9dce63318fe88b3
buriihenry <buriihenry@gmail.com>
2022-09-14 07:46:52 +0300

    Airflow

--- Week 2/airflow/dags_new/data_ingestion_local.py
+++ Week 2/airflow/dags_new/data_ingestion_local.py
@@ -10,6 +10,8 @@ from airflow.operators.python import PythonOperator
 
 local_workflow = DAG(
     "LocalIngestionDag",
+    schedule_interval="0 6 2 * *",
+    start_date=datetime(2021, 1, 1)
     
 )
 
@@ -22,7 +24,7 @@ with local_workflow:
 
     ingest_task = BashOperator(
         task_id='ingest',
-        bash_command='echo "hello-world"'
+        bash_command='echo "pwd"'
     )
 
     wget_task >> ingest_task
