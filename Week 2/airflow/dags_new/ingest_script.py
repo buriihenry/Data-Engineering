109961e048822473a0ea82ca12e0a0793d6e78cc
buriihenry <buriihenry@gmail.com>
2022-09-17 17:54:54 +0300

    Airflow +Docker + Sql

--- Week 2/airflow/dags_new/ingest_script.py
+++ Week 2/airflow/dags_new/ingest_script.py
@@ -14,35 +14,35 @@ def ingest_callable(user, password, host, port, db, table_name, parquet_file, ex
 
     print('connection established successfully, inserting data...')
 
-    t_start = time()
-    df_iter = pd.read_parquet(parquet_file, iterator=True)
+    # t_start = time()
+    # df_iter = pd.read_parquet(parquet_file, iterator=True)
 
-    df = next(df_iter)
+    # df = next(df_iter)
 
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    # # df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    # # df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    # df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
-    df.to_sql(name=table_name, con=engine, if_exists='append')
+    # df.to_sql(name=table_name, con=engine, if_exists='append')
 
-    t_end = time()
-    print('inserted the first chunk, took %.3f second' % (t_end - t_start))
+    # # t_end = time()
+    # # print('inserted the first chunk, took %.3f second' % (t_end - t_start))
 
-    while True: 
-        t_start = time()
+    # # while True: 
+    # #     t_start = time()
 
-        try:
-            df = next(df_iter)
-        except StopIteration:
-            print("completed")
-            break
+    # #     try:
+    # #         df = next(df_iter)
+    # #     except StopIteration:
+    # #         print("completed")
+    # #         break
 
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    # #     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    # #     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-        df.to_sql(name=table_name, con=engine, if_exists='append')
+    # #     df.to_sql(name=table_name, con=engine, if_exists='append')
 
-        t_end = time()
+    # #     t_end = time()
 
-        print('inserted another chunk, took %.3f second' % (t_end - t_start))
+    # #     print('inserted another chunk, took %.3f second' % (t_end - t_start))
