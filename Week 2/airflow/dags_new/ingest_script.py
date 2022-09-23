895845273aa4111c61e00df2952a515b19aabe91
buriihenry <buriihenry@gmail.com>
2022-09-23 08:51:19 +0300

    Airflow

--- Week 2/airflow/dags_new/ingest_script.py
+++ Week 2/airflow/dags_new/ingest_script.py
@@ -19,30 +19,30 @@ def ingest_callable(user, password, host, port, db, table_name, parquet_file, ex
 
     df = next(df_iter)
 
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    # df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    # df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
-    t_end = time()
-    print('inserted the first chunk, took %.3f second' % (t_end - t_start))
+    # t_end = time()
+    # print('inserted the first chunk, took %.3f second' % (t_end - t_start))
 
-    while True: 
-        t_start = time()
+    # while True: 
+    #     t_start = time()
 
-        try:
-            df = next(df_iter)
-        except StopIteration:
-            print("completed")
-            break
+    #     try:
+    #         df = next(df_iter)
+    #     except StopIteration:
+    #         print("completed")
+    #         break
 
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    #     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    #     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-        df.to_sql(name=table_name, con=engine, if_exists='append')
+    #     df.to_sql(name=table_name, con=engine, if_exists='append')
 
-        t_end = time()
+    #     t_end = time()
 
-        print('inserted another chunk, took %.3f second' % (t_end - t_start))
+    #     print('inserted another chunk, took %.3f second' % (t_end - t_start))
