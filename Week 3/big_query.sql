5c77d2026dca6ed6e370c5ca7c7f3d1cf578f77c
buriihenry <buriihenry@gmail.com>
2022-09-26 05:39:49 +0300

    Data Warehouse and Big Query

--- Week 3/big_query.sql
+++ Week 3/big_query.sql
@@ -1,4 +1,5 @@
 -- Big Query SQL 
+-- bamboo-autumn-360913 (Project_name)
 -- Creating external table referring to gcs path
 CREATE OR REPLACE EXTERNAL TABLE `bamboo-autumn-360913.nytaxi.external_yellow_tripdata`
 OPTIONS (
