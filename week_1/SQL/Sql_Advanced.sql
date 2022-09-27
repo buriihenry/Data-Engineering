ad91c242584556b0b471efe28038dd06b722c053
buriihenry <buriihenry@gmail.com>
2022-09-27 04:25:07 +0300

    Docker + sql

--- week_1/SQL/Sql_Advanced.sql
+++ week_1/SQL/Sql_Advanced.sql
@@ -3,11 +3,20 @@ Given a table of candidates and their skills, you're tasked with finding the can
 Write a SQL query to list the candidates who possess all of the required skills for the job. Sort the the output by candidate ID in ascending order.
 
 Assumption:There are no duplicates in the candidates table.
-/*QUery*/
 
+/*QUery*/
 SELECT candidate_id
 FROM candidates
 WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
 GROUP BY candidate_id
 HAVING COUNT(skill) = 3
 ORDER BY candidate_id;
+
+Assume you are given the tables below about Facebook pages and page likes. Write a query to return the page IDs of all the Facebook pages that don't have any likes. The output should be in ascending order.
+
+/*QUery 2*/
+SELECT pages.page_id
+FROM pages
+LEFT OUTER JOIN page_likes AS likes
+  ON pages.page_id = likes.page_id
+WHERE likes.page_id IS NULL;
