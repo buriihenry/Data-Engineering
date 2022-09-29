e4d6dd845b038da87087ebcdfd150f43aa58aa61
buriihenry <buriihenry@gmail.com>
2022-09-29 05:36:35 +0300

    Big Query

---
+++ Week 3/big_query.sql
@@ -0,0 +1,15 @@
+
+-- Creating external table referring to gcs path
+CREATE OR REPLACE EXTERNAL TABLE `bamboo-autumn-360913.nytaxi.external_yellow_tripdata`
+OPTIONS (
+  format = 'parquet',
+  uris = ['gs://transfer_service_gcp/trip data/yellow_tripdata_2019-*.parquet', 'gs://transfer_service_gcp/trip data/yellow_tripdata_2020-*.parquet']
+);
+
+-- Check yellow trip data
+
+SELECT * except(airport_fee) FROM `bamboo-autumn-360913.nytaxi.external_yellow_tripdata` LIMIT 1000;
+
+-- Create a non partitioned table from external table
+CREATE OR REPLACE TABLE bamboo-autumn-360913.nytaxi.yellow_tripdata_non_partitoned AS
+SELECT * except(airport_fee) FROM bamboo-autumn-360913.nytaxi.external_yellow_tripdata;
