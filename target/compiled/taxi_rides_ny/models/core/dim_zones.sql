19546cd674b242348c3b51e359839bd28c6424fd
Burii <buriihenry@gmail.com>
2022-10-12 19:05:06 +0300

    Delete target directory

--- target/compiled/taxi_rides_ny/models/core/dim_zones.sql
+++
@@ -1,8 +0,0 @@
-
-
-select 
-    locationid,
-    borough,
-    zone,
-    replace(service_zone,'Boro','Green') as service_zone
-from `bamboo-autumn-360913`.`dbt_buriihenry`.`taxi_zone_lookup`
--- target/compiled/taxi_rides_ny/models/example/my_second_dbt_model.sql
+++
@@ -1,5 +0,0 @@
--- Use the `ref` function to select from other models
-
-select *
-from `bamboo-autumn-360913`.`dbt_buriihenry`.`my_first_dbt_model`
-where id = 1
