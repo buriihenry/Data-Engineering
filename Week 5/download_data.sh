ec54673d2a72005f1585d677a3ec194b4ead24f2
buriihenry <buriihenry@gmail.com>
2022-10-19 18:02:13 +0300

    Upload Script

--- Week 5/download_data.sh
+++ Week 5/download_data.sh
@@ -3,17 +3,16 @@ set -e
 TAXI_TYPE=$1 # "green"
 YEAR=$2 # 2020
 
-URL_PREFIX=" https://d37ci6vzurychx.cloudfront.net/trip-data"
-
+URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/tag/yellow"
 
 
 for MONTH in {1..12}; do
   FMONTH=`printf "%02d" ${MONTH}`
 
-  URL="${URL_PREFIX}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.parquet"
+  URL="${URL_PREFIX}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv"
 
   LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
-  LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.parquet"
+  LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.csv"
   LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
 
   echo "donwloading ${URL} to ${LOCAL_PATH}"
