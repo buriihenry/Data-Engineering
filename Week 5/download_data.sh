6d49711a3b3098d0bd52bf84d9823059f0893879
buriihenry <buriihenry@gmail.com>
2022-10-22 09:59:43 +0300

    download script

--- Week 5/download_data.sh
+++ Week 5/download_data.sh
@@ -3,12 +3,14 @@ set -e
 TAXI_TYPE=$1 # "yellow"
 YEAR=$2 # 2020
 
-URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/tag/yellowx"
+URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/tag/yellow"
+#URL_PREFIX="https://s3.amazonaws.com/nyc-tlc/trip+data"
+https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet
 
 for MONTH in {1..12}; do
   FMONTH=`printf "%02d" ${MONTH}`
 
-  URL="${URL_PREFIX}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv"
+  URL="${URL_PREFIX}/${TAXI_TYPE}/${YEAR}-${FMONTH}.csv"
 
   LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
   LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.csv"
